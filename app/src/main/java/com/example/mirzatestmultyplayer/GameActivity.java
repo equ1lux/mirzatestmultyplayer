59af5fce49414c62c380c941150384e5448abaee
equ1lux <mirza.maznikar@gmail.com>
2015-04-18 17:09:21 +0200

    joinRoomAdded

--- app/src/main/java/com/example/mirzatestmultyplayer/GameActivity.java
+++ app/src/main/java/com/example/mirzatestmultyplayer/GameActivity.java
@@ -1,16 +1,173 @@
 package com.example.mirzatestmultyplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class GameActivity extends Activity{
+import com.shephertz.app42.gaming.multiplayer.client.WarpClient;
+import com.shephertz.app42.gaming.multiplayer.client.events.ChatEvent;
+import com.shephertz.app42.gaming.multiplayer.client.events.LiveRoomInfoEvent;
+import com.shephertz.app42.gaming.multiplayer.client.events.LobbyData;
+import com.shephertz.app42.gaming.multiplayer.client.events.MoveEvent;
+import com.shephertz.app42.gaming.multiplayer.client.events.RoomData;
+import com.shephertz.app42.gaming.multiplayer.client.events.RoomEvent;
+import com.shephertz.app42.gaming.multiplayer.client.events.UpdateEvent;
+import com.shephertz.app42.gaming.multiplayer.client.listener.NotifyListener;
+import com.shephertz.app42.gaming.multiplayer.client.listener.RoomRequestListener;
+
+import java.util.HashMap;
+
+public class GameActivity extends Activity implements RoomRequestListener, NotifyListener {
+    private WarpClient theClient;
+    private String roomId = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-	}
-	
+        Intent intent = getIntent();
+        roomId = intent.getStringExtra("roomId");
+        init(roomId);
 
+	}
+
+    private void init(String roomId){
+        if(theClient!=null){
+            theClient.addRoomRequestListener(this);
+            theClient.addNotificationListener(this);
+            theClient.joinRoom(roomId);
+        }
+    }
+
+
+    @Override
+    public void onRoomCreated(RoomData roomData) {
+
+    }
+
+    @Override
+    public void onRoomDestroyed(RoomData roomData) {
+
+    }
+
+    @Override
+    public void onUserLeftRoom(RoomData roomData, String s) {
+
+    }
+
+    @Override
+    public void onUserJoinedRoom(RoomData roomData, String s) {
+
+    }
+
+    @Override
+    public void onUserLeftLobby(LobbyData lobbyData, String s) {
+
+    }
+
+    @Override
+    public void onUserJoinedLobby(LobbyData lobbyData, String s) {
+
+    }
+
+    @Override
+    public void onChatReceived(ChatEvent chatEvent) {
+
+    }
+
+    @Override
+    public void onPrivateChatReceived(String s, String s2) {
+
+    }
+
+    @Override
+    public void onPrivateUpdateReceived(String s, byte[] bytes, boolean b) {
+
+    }
+
+    @Override
+    public void onUpdatePeersReceived(UpdateEvent updateEvent) {
+
+    }
+
+    @Override
+    public void onUserChangeRoomProperty(RoomData roomData, String s, HashMap<String, Object> stringObjectHashMap, HashMap<String, String> stringStringHashMap) {
+
+    }
+
+    @Override
+    public void onMoveCompleted(MoveEvent moveEvent) {
+
+    }
+
+    @Override
+    public void onGameStarted(String s, String s2, String s3) {
+
+    }
+
+    @Override
+    public void onGameStopped(String s, String s2) {
+
+    }
+
+    @Override
+    public void onUserPaused(String s, boolean b, String s2) {
+
+    }
+
+    @Override
+    public void onUserResumed(String s, boolean b, String s2) {
+
+    }
+
+    @Override
+    public void onNextTurnRequest(String s) {
+
+    }
+
+    @Override
+    public void onSubscribeRoomDone(RoomEvent roomEvent) {
+
+    }
+
+    @Override
+    public void onUnSubscribeRoomDone(RoomEvent roomEvent) {
+
+    }
+
+    @Override
+    public void onJoinRoomDone(RoomEvent roomEvent) {
+
+    }
+
+    @Override
+    public void onLeaveRoomDone(RoomEvent roomEvent) {
+
+    }
+
+    @Override
+    public void onGetLiveRoomInfoDone(LiveRoomInfoEvent liveRoomInfoEvent) {
+
+    }
+
+    @Override
+    public void onSetCustomRoomDataDone(LiveRoomInfoEvent liveRoomInfoEvent) {
+
+    }
+
+    @Override
+    public void onUpdatePropertyDone(LiveRoomInfoEvent liveRoomInfoEvent) {
+
+    }
+
+    @Override
+    public void onLockPropertiesDone(byte b) {
+
+    }
+
+    @Override
+    public void onUnlockPropertiesDone(byte b) {
+
+    }
 }
